7d9a8562e2b29827b1fc53323f0f93ecb6981a73
Bisal Saha <71567584+rudro1@users.noreply.github.com>
2026-08-03 03:44:30 +0600

    Update page.tsx

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -14,15 +14,8 @@ export const metadata: Metadata = {
   },
 };
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZJDVM7SR0H"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-ZJDVM7SR0H');
-</script>
+
 export const POSTS = [
   {
     slug: "how-to-remove-background-from-image-free",
